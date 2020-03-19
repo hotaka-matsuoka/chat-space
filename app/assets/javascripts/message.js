996fe5197b5268c0ed4f3c8bbd2e43567eb07e5f
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-03-19 18:28:02 +0900

    add automatic-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTNL(message){
     if (message.image) {
       var html =  
-        `<div class="main-chat__message-list--message">
+        `<div class="main-chat__message-list--message" data-message-id = ${message.id}>
           <div class="message-info">
             <div class="message-info__member-name">
               ${message.user_name}
@@ -19,7 +20,7 @@ $(function(){
       return html;
     } else {
       var html =
-        `<div class="main-chat__message-list--message">
+        `<div class="main-chat__message-list--message" data-message-id = ${message.id}>
           <div class="message-info">
             <div class="message-info__member-name">
               ${message.user_name}
@@ -61,4 +62,30 @@ $(function(){
       $('.send-btn').removeAttr("disabled");
     });
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat__message-list--message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTNL(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 1000);
+  }
 });
